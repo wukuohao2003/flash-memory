408ff46604ccfea011ae2573b3b9caab7cdc9a3b
user <2735999294@qq.com>
2024-03-13 20:09:49 +0800

    完成邮箱

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 export default function App() {
+
   return (
     <div style={{ height: '100%', width: '100%',boxSizing:'border-box' }}>
       <Outlet></Outlet>
